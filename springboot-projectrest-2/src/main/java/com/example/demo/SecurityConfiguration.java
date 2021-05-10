ec257b215fb92067f134905c83f67667dd4e3e0f
babji <babji@DESKTOP-2VFRJ0D>
2021-05-10 12:52:47 +0530

    hasAnyRole -- let admin acces both /user and /admin resources

--- springboot-projectrest-2/src/main/java/com/example/demo/SecurityConfiguration.java
+++ springboot-projectrest-2/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -30,11 +30,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/**")//match for all the paths
-		.hasRole("ADMIN")
+		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/user").hasAnyRole("USER","ADMIN")//please allow access to /user url to both user and admin
+		.antMatchers("/").permitAll()
 		//.hasAnyRole("_","_")
 		.and()
 		.formLogin();
 	}
-
+	
 }
